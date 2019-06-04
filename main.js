3d6800b28e812ef26f4f055b09427b9fc19a3330
pixldemon <moritzamando@gmail.com>
2019-06-04 14:24:09 +0200

    improved layout, added booking site

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 let lastScrollTop = 10000;
-let header;
+let header, spacer;
 
 function onload() {
 	header = document.getElementsByTagName("header")[0];
+	spacer = document.getElementById("spacer");
 	document.body.classList.remove("preload");
+	document.body.onscroll = e => {
+
+		let scrollTop = document.documentElement.scrollTop || window.pageYOffset;
+	
+		if(scrollTop > 0){
+			header.classList.add("scrolled");
+			// spacer.style.width = "50vw";
+		} else {
+			header.classList.remove("scrolled");
+			// spacer.style.width = "";
+		}
+	};
+	document.body.onscroll()
 }
-
-document.addEventListener("scroll", e => {
-
-	let scrollTop = document.documentElement.scrollTop || window.pageYOffset;
-
-	if(scrollTop > 0){
-		header.classList.add("scrolled");
-	} else {
-		header.classList.remove("scrolled");
-	}
-});
