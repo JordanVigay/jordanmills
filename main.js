45b0ad736f453d30bdc27d9be34d3dc52fa18b04
JordanVigay <51373105+JordanVigay@users.noreply.github.com>
2024-07-25 06:14:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let header = document.getElementsByTagName("header")[0];
-    let spacer = document.getElementById("spacer");
-    let arrow = document.getElementById('arrow');
+document.addEventListener('DOMContentLoaded', () => {
+    const arrow = document.getElementById('arrow');
 
-    // Function to handle scroll event
+    if (!arrow) {
+        console.error('Arrow element not found');
+        return;
+    }
+
     function onScroll() {
-        let scrollTop = document.documentElement.scrollTop || window.pageYOffset;
-
-        // Handle header scroll behavior
-        if (scrollTop > 0) {
-            header.classList.add("scrolled");
-            // spacer.style.width = "50vw";
-        } else {
-            header.classList.remove("scrolled");
-            // spacer.style.width = "";
-        }
-
-        // Handle arrow opacity
         if (window.scrollY > 20) {
             arrow.style.opacity = '0';
         } else {
@@ -24,9 +14,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
-    // Attach the scroll event handler to the window
     window.addEventListener('scroll', onScroll);
-
-    // Initial check in case the page is already scrolled
-    onScroll();
+    onScroll(); // Initial check
 });
